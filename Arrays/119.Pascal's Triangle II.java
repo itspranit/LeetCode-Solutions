d0ffb9c0dedbb61a7ba5627d49c61f6de01f51ab
itspranit <pranitrathkanthiwar123@gmail.com>
2025-08-31 22:43:44 +0530

    Create 119.Pascal's Triangle II.java

---
+++ Arrays/119.Pascal's Triangle II.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);  
+            
+            for (int j = 1; j < i; j++) {
+                int value = triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j);
+                row.add(value);
+            }
+            
+            if (i > 0) {
+                row.add(1);  
+            }
+            
+            triangle.add(row);
+        }
+
+        return triangle.get(rowIndex);
+    }
+}
