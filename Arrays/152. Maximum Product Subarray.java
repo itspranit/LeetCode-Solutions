55c7c134003e6afb627e3b83592bda8d9f930833
itspranit <pranitrathkanthiwar123@gmail.com>
2026-03-20 22:31:12 +0530

    Create 152. Maximum Product Subarray.java

---
+++ Arrays/152. Maximum Product Subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = maxSoFar;
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            
+            // Temporary variable to store maxSoFar because it will be updated
+            // before we calculate minSoFar
+            int tempMax = Math.max(curr, Math.max(maxSoFar * curr, minSoFar * curr));
+            minSoFar = Math.min(curr, Math.min(maxSoFar * curr, minSoFar * curr));
+            
+            maxSoFar = tempMax;
+            result = Math.max(result, maxSoFar);
+        }
+
+        return result;
+    }
+}
