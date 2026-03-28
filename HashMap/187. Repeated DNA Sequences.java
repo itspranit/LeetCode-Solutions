c81f2a3f4e644d168dbb8f6a8513307964f19870
itspranit <pranitrathkanthiwar123@gmail.com>
2026-03-28 23:43:02 +0530

    Create 187. Repeated DNA Sequences.java

---
+++ HashMap/187. Repeated DNA Sequences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+            int lengthSequence = 10;
+            List<String> res = new ArrayList<String>();
+            //List<String> checkSequence = new ArrayList<>();
+            HashSet<String> resHash = new HashSet<>();
+            for (int i = 0; i <= s.length()-lengthSequence; i++) {
+                String resStr = s.substring(i,i+lengthSequence);
+                boolean isNew = resHash.add(resStr);
+                if (!isNew && !res.contains(resStr)) {
+
+                    res.add(resStr);
+                }
+            }
+            return res;
+        }
+}
