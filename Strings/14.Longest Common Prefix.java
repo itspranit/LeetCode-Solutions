23802aa8f1fc631565ecd5859277212482615520
itspranit <pranitrathkanthiwar123@gmail.com>
2026-04-30 23:14:31 +0530

    Clean up formatting in longestCommonPrefix method
    Removed unnecessary blank lines in the longestCommonPrefix method.

--- Strings/14.Longest Common Prefix.java
+++ Strings/14.Longest Common Prefix.java
@@ -4,8 +4,6 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         int inx = 0; 
         Arrays.sort(strs);
-
-       
         String s1 = strs[0]; 
         String s2 = strs[strs.length - 1]; 
 
