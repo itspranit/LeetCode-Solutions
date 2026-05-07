32b8112dbdb86b8b4bfd19336d45599ab634aa5a
itspranit <pranitrathkanthiwar123@gmail.com>
2026-05-07 23:33:31 +0530

    Update 11.Container With Most Photos.java

--- Arrays/11.Container With Most Photos.java
+++ Arrays/11.Container With Most Photos.java
@@ -4,8 +4,7 @@ class Solution {
         int area=0; // Variable to store the maximum area
         int i=0; // Left pointer starting at the beginning
         int j=n-1; // Right pointer starting at the end
-        
-        // Loop until the two pointers meet
+
         while (i < j) {
             // Calculate the current area and update the maximum area
             area = Math.max(area, Math.min(height[i], height[j]) * (j - i));
