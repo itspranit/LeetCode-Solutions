23219c3ce35ba73f477281b6d7d8fe9093dd9d14
itspranit <pranitrathkanthiwar123@gmail.com>
2026-05-08 22:50:43 +0530

    Update 160. Intersection of Two Linked Lists.java

--- HashMap/160. Intersection of Two Linked Lists.java
+++ HashMap/160. Intersection of Two Linked Lists.java
@@ -14,12 +14,10 @@ public class Solution {
         
         ListNode A = headA;
         ListNode B = headB;
-
         while (A != B) {
             A = (A != null) ? A.next : headB;
             B = (B != null) ? B.next : headA;
         }
-
         return A; 
     }
 }
